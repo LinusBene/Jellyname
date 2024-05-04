b43d557fcc5d8aae6a07132e0bfd91b3e5fdd8ab
Linus <linus.witte@proton.me>
2024-05-04 18:55:14 +0200

    added recursive calls

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ void print_help() {
     printf("  -h Print this help message\n");
     printf("  -v Print version information\n");
     printf("  -y yes to all\n");
+    printf("  -r recursive calls to subdirectories\n");
 }
 
 int is_video_file(const char *filename) {
@@ -32,19 +33,22 @@ int is_video_file(const char *filename) {
     return 0; 
 }
 
-uint16_t getNumber(const char* str) {
+uint8_t getNumber(const char* str, uint16_t* res) {
     
+    if(str[0] < '0' || str[0] > '9') 
+        return EXIT_FAILURE;
+
     uint8_t i;
     for(i = 0; str[i] != '\0'; i++) {
-        if(str[i + 1] < '0' && str[i + 1] > '9' && str[i + 2] < '0' && str[i + 2] > '9') {
-            break; 
+        if(str[i] < '0' || str[i] > '9') {
+            break;
         }
     }
 
     char numbers[i];
     strncpy(numbers, str, i);
-
-    return (uint16_t) atoi(numbers);
+    *res = (uint16_t) atoi (numbers);
+    return EXIT_SUCCESS;
 }
 
 uint8_t userConfirm(const char* msg) {
@@ -61,17 +65,23 @@ char* getNewName(const char* currentName, const char* pattern) {
 
     const char *extension = strrchr(currentName, '.');
 
-    int16_t season = -1;
-    int16_t episode = -1;
+    uint16_t season = 0xFFFF;
+    uint16_t episode = 0xFFFF;
     for(int i = 0; currentName[i] != '\0'; i++) {
-        if(currentName[i] == 'S') {
-            season = getNumber(&currentName[i + 1]);
-        }else if (currentName[i] == 'E') {
-            episode = getNumber(&currentName[i + 1]);
+        if(currentName[i] == 'S' && season == 0xFFFF) {
+            if(getNumber(&currentName[i+1], &season) == EXIT_SUCCESS) {
+                if(episode != 0xFFFF)
+                    break;
+            }
+        }else if (currentName[i] == 'E' && episode == 0xFFFF) {
+            if(getNumber(&currentName[i+1], &episode) == EXIT_SUCCESS) {
+                if(season != 0xFFFF)
+                    break;
+            }
         }
     }
 
-    if(season == -1 || episode == -1)
+    if(season == 0xFFFF || episode == 0xFFFF)
         return "null";
 
     const size_t resLength = strlen(extension) + strlen(currentName) + 9;
@@ -86,39 +96,7 @@ char* getNewName(const char* currentName, const char* pattern) {
     return res;
 }
 
-int main(int argc, char * argv[]) {
-
-    int opt;
-    uint8_t approvedFlag = 0;
-
-    while ((opt = getopt(argc, argv, "hvy")) != -1) {
-        switch (opt) {
-            case 'h':
-                print_help();
-                return 0;
-            case 'v':
-                printf("Version 1.0\n");
-                return 0;
-            case 'y':
-                approvedFlag = 1;
-                break;
-            case '?':
-                fprintf(stderr, "Invalid option: -%c\n", optopt);
-                print_help();
-                return 1;
-            default:
-                // Should not reach here
-                abort();
-        }
-    }
-    char* pattern = approvedFlag ? argv[2] : argv [1];
-    char* dirTarget = approvedFlag ? argv[3] : argv [2];
-
-    if(argc < 3 || argc > 4) {
-        fprintf(stderr, "Exactly two arguments are required\n");
-        print_help();
-    }
-
+int renameFiles(const char* pattern, const char* dirTarget, uint8_t recursiveFlag, uint8_t approvedFlag) {
     DIR *dir; 
 
     struct dirent *entry;
@@ -130,40 +108,111 @@ int main(int argc, char * argv[]) {
         return EXIT_FAILURE;
     }
 
+    size_t dirPathLength = strlen(dirTarget);
+    
+    char filePath[dirPathLength + 150]; //set max file name length to 150
+    strcpy(filePath, dirTarget);
+
+    // appending '/' to end if dir path
+    if(dirTarget[dirPathLength - 1] != '/'){
+        filePath[dirPathLength] = '/';
+        filePath[dirPathLength+1] = '\0';
+        dirPathLength = dirPathLength + 1;
+    }
+
+    char newFilePath[dirPathLength + 150]; //set max file name length to 150
+    strcpy(newFilePath, filePath);
+    
     while ((entry = readdir(dir)) != NULL) {
         // Ignore "." and ".." entries
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
             continue;
 
+        strcpy(filePath + dirPathLength, entry->d_name);
+
         // Get file information
-        if (lstat(entry->d_name, &statbuf) == -1) {
+        if (lstat(filePath, &statbuf) == -1) {
             fprintf(stderr, "Error getting file information\n");
             continue;
         }
 
         if (S_ISREG(statbuf.st_mode)) {
             if(is_video_file(entry->d_name)){
-                char* newFileName = getNewName(entry->d_name, pattern);
+                char *newFileName = getNewName(entry->d_name, pattern);
+
+                if(strcmp(newFileName, "null") == 0){
+                    continue;
+                }
+
+                strcpy(&newFilePath[dirPathLength], newFileName);
                 uint8_t approved = approvedFlag;
                 
                 if(!approved) {
-                    char * temp = "Rename '%s' to '%s'? [Y/n]?\t";
+                    char * temp = "Rename '%s' to '%s'? [y/n]?\t";
                     char msg[strlen(temp) + strlen(entry->d_name) + strlen(newFileName)];
                     sprintf(msg, temp, entry->d_name, newFileName);
                     approved = userConfirm(msg);
                 }
 
-                if(approved){
-                    rename(entry->d_name, newFileName);
+                if(approved && strcmp(newFileName, "null") != 0){
+                    int res = rename(filePath, newFilePath);
+                    if(res == -1) {
+                        fprintf(stderr, "Error renaming the file\n");
+                    }
                 }
 
                 free(newFileName);
             }
+        }else if (recursiveFlag && S_ISDIR(statbuf.st_mode)) {
+            renameFiles(pattern, filePath, recursiveFlag, approvedFlag);
         }
     }
 
     // Close the directory
     closedir(dir);
-
+    
     return EXIT_SUCCESS;
+}
+
+int main(int argc, char * argv[]) {
+    int opt;
+    uint8_t approvedFlag = 0;
+    uint8_t recursiveFlag = 0;
+    uint8_t optionsSet = 0;
+
+    while ((opt = getopt(argc, argv, "hvyr")) != -1) {
+        switch (opt) {
+            case 'h':
+                print_help();
+                return 0;
+            case 'v':
+                printf("Version 1.0\n");
+                return 0;
+            case 'y':
+                approvedFlag = 1;
+                optionsSet = optionsSet + 1;
+                break;
+            case 'r':
+                optionsSet = optionsSet + 1;
+                recursiveFlag = 1;
+                break;
+            case '?':
+                fprintf(stderr, "Invalid option: -%c\n", optopt);
+                print_help();
+                return 1;
+            default:
+                // Should not reach here
+                abort();
+        }
+    }
+
+    char* pattern = argv[1 + optionsSet];
+    char* dirTarget = argv[2 + optionsSet];
+
+    if(argc - 1 - optionsSet != 2) {
+        fprintf(stderr, "Exactly two arguments are required\n");
+        print_help();
+    }
+
+    return renameFiles(pattern, dirTarget, recursiveFlag, approvedFlag);
 }
